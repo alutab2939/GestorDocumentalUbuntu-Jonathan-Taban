8b5d81741b1452a63de3752917f0d940c8caed25
alutab2939 <91492963+alutab2939@users.noreply.github.com>
2023-02-13 00:50:12 +0100

    Update extra.sh

--- Scripts/extra.sh
+++ Scripts/extra.sh
@@ -15,5 +15,5 @@ sudo useradd -m -s /bin/bash -G gr_Prof_rx usu_Prof
 #Añadir Usuarios a Grupos
 sudo usermod -a -G gr_1ESO_rx,gr_2ESO_rx,gr_3ESO_rx,gr_4ESO_rx alu_ESO
 sudo usermod -a -G gr_1BACH_rx,gr_2BACH_rx alu_BACH
-usermsudo od -a -G gr_DAM_rx alu_DAM
+sudo usermod -a -G gr_DAM_rx alu_DAM
 sudo usermod -a -G gr_1ESO_rx,gr_2ESO_rx,gr_3ESO_rx,gr_4ESO_rx,gr_1BACH_rx,gr_2BACH_rx,gr_DAM_rx usu_Prof
