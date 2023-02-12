940e3e361780c2c65a54eaac6ce50d36e95ce305
alutab2939 <91492963+alutab2939@users.noreply.github.com>
2023-02-13 00:25:42 +0100

    Update extra.sh

--- Scripts/extra.sh
+++ Scripts/extra.sh
@@ -7,19 +7,13 @@ sudo groupadd gr_DAM_rx
 sudo groupadd gr_Prof_rx
 
 #Crear Usuarios
-sudo useradd -g gr_ESO_rx -d /home/alu_ESO -m -s /bin/bash alu_ESO
-sudo useradd -g gr_BACH_rx -d /home/alu_BACH -m -s /bin/bash alu_BACH
-sudo useradd -g gr_DAM_rx -d /home/alu_DAM -m -s /bin/bash alu_DAM
-sudo useradd -g gr_Prof_rx -d /home/usu_Prof -m -s /bin/bash usu_Prof
+sudo useradd -m -s /bin/bash -G gr_ESO_rx alu_ESO
+sudo useradd -m -s /bin/bash -G gr_BACH_rx alu_BACH
+sudo useradd -m -s /bin/bash -G gr_DAM_rx alu_DAM
+sudo useradd -m -s /bin/bash -G gr_Prof_rx usu_Prof
 
 #Añadir Usuarios a Grupos
-sudo usermod -a -G alu_ESO  gr_1ESO_rx,gr_2ESO_rx,gr_3ESO_rx,gr_4ESO_rx
-sudo usermod -a -G alu_BACH gr_1BACH_rx,gr_2BACH_rx
-usermsudo od -a -G alu_DAM gr_DAM_rx
-sudo usermod -a -G usu_Prof gr_1ESO_rx,gr_2ESO_rx,gr_3ESO_rx,gr_4ESO_rx,gr_1BACH_rx,gr_2BACH_rx,gr_DAM_rx
-
-#Cambiar contraseña
-passwd alu_ESO
-passwd alu_BACH
-passwd alu_DAM
-passwd usu_Prof
+sudo usermod -a -G gr_1ESO_rx,gr_2ESO_rx,gr_3ESO_rx,gr_4ESO_rx alu_ESO
+sudo usermod -a -G gr_1BACH_rx,gr_2BACH_rx alu_BACH
+usermsudo od -a -G gr_DAM_rx alu_DAM
+sudo usermod -a -G gr_1ESO_rx,gr_2ESO_rx,gr_3ESO_rx,gr_4ESO_rx,gr_1BACH_rx,gr_2BACH_rx,gr_DAM_rx usu_Prof
